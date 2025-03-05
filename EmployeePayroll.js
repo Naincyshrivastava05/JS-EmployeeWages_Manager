d9b9a53f92dca5327950b28ee3fbf5639e604620
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-03-05 22:38:15 +0530

    [Naincy] add. JS EmployeeWages Ability to check the employee id and salary are non zero positive number, the gender is M or F and date is not a future date

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -10,11 +10,15 @@ class EmployeePayrollData {
       this.salary = salary;
   }
       constructor(...params) {
-        this.id = params[0];
-        this.name = params[1];
-        this.salary = params[2];
-        this.gender = params[3];
-        this.startDate = params[4];
+        try {
+          this.id = this.validateId(params[0]);
+          this.name = this.validateName(params[1]);
+          this.salary = this.validateSalary(params[2]);
+          this.gender = this.validateGender(params[3]);
+          this.startDate = this.validateStartDate(params[4]);
+      } catch (error) {
+          console.error(error);
+      }
   }
 
 
@@ -26,6 +30,31 @@ class EmployeePayrollData {
         this._name = name;
     else throw 'Name is Incorrect!';
 }
+   validateId(id) {
+            if (id > 0) return id;
+            throw 'ID must be a positive number!';
+        }
+    
+        validateName(name) {
+            let nameRegex = RegExp('^[A-Z][a-z]+$');
+            if (nameRegex.test(name)) return name;
+            throw 'Name is Incorrect!';
+        }
+    
+        validateSalary(salary) {
+            if (salary > 0) return salary;
+            throw 'Salary must be a positive number!';
+        }
+    
+        validateGender(gender) {
+            if (gender === 'M' || gender === 'F') return gender;
+            throw "Gender must be 'M' or 'F'!";
+        }
+    
+        validateStartDate(startDate) {
+            if (startDate instanceof Date && startDate <= new Date()) return startDate;
+            throw 'Start Date cannot be in the future!';
+        }
 
   toString() {
       return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary;
@@ -39,7 +68,15 @@ class EmployeePayrollData {
 let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
 
+try {
+  let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000, "M", new Date());
+  console.log(employeePayrollData.toString());
+} catch (e) {
+  console.error(e);
+}
+
 employeePayrollData.name = "John";
+let employeePayrollData = new EmployeePayrollData(2, "john", 30000, "M", new Date());
 console.log(employeePayrollData.toString());
 try {
   employeePayrollData.name = "Naincy";
@@ -50,3 +87,10 @@ try {
 
 let newEmployeePayrollData = new EmployeePayrollData(2, "Terrisa", 30000, "F", new Date());
 console.log(newEmployeePayrollData.toString());
+
+try {
+  let newEmployeePayrollData = new EmployeePayrollData(-1, "Terrisa", -30000, "X", new Date("2025-01-01"));
+  console.log(newEmployeePayrollData.toString());
+} catch (e) {
+  console.error(e);
+}
