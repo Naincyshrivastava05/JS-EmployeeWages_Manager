74d5eb1eb82388dee3bbe249d1dea12817a026e4
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-03-05 22:21:54 +0530

    [Naincy] add JS EmployeeWages Ability to extend Employee Payroll Data to store gender and start date

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -9,7 +9,7 @@ class EmployeePayrollData {
       this.name = name;
       this.salary = salary;
   }
-      constructor(params) {
+      constructor(...params) {
         this.id = params[0];
         this.name = params[1];
         this.salary = params[2];
@@ -20,11 +20,18 @@ class EmployeePayrollData {
 
   get name() { return this._name; }
   set name(name) { this._name = name; }
+  set name(name) {
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+    if (nameRegex.test(name))
+        this._name = name;
+    else throw 'Name is Incorrect!';
+}
 
   toString() {
       return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary;
       const options = { year: 'numeric', month: 'long', day: 'numeric' };
-      const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+   
+      const empDate = this.startDate ? this.startDate.toLocaleDateString("en-US", options) : "undefined";
       return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary + ", gender=" + this.gender + ", startDate=" + empDate;
   }
 }
@@ -34,6 +41,12 @@ console.log(employeePayrollData.toString());
 
 employeePayrollData.name = "John";
 console.log(employeePayrollData.toString());
+try {
+  employeePayrollData.name = "Naincy";
+  console.log(employeePayrollData.toString());
+} catch (e) {
+  console.error(e);
+}
 
-let newEmployeePayrollData = new EmployeePayrollData([2, "Terrisa", 30000, "F", new Date()]);
+let newEmployeePayrollData = new EmployeePayrollData(2, "Terrisa", 30000, "F", new Date());
 console.log(newEmployeePayrollData.toString());
